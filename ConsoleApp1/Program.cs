ba9fc070ae03ce8ee721f8b4eb9d79d93a58fdc3
Eugene <samsonov_eugene@mail.ru>
2019-01-10 10:20:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.IO.Ports;
+
+namespace SerialPortExample
+{
+    class SerialPortProgram
+    { 
+        private SerialPort port = new SerialPort("COM2", 9600, Parity.None, 8, StopBits.One);
+        private int count = 1;
+        private XLWorkbook workbook = new XLWorkbook();
+        private IXLWorksheet worksheet = null;
+
+        static void Main(string[] args)
+        {
+            new SerialPortProgram();
+        }
+
+        private SerialPortProgram()
+        {
+            worksheet = workbook.Worksheets.Add("Sample");
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            port.Open();
+            Console.ReadKey();
+            workbook.SaveAs("C:\\Users\\User\\Desktop\\docs\\test.xlsx");
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var buffer = new byte[port.ReadBufferSize];
+            port.Read(buffer, 0, buffer.Length);
+            Console.WriteLine(System.Text.Encoding.Default.GetString(buffer));
+            worksheet.Cell("A" + count).Value = System.Text.Encoding.Default.GetString(buffer);
+            count++;
+        }
+    }
+}
