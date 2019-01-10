a86d01e8304e61229523a6cbae89fddbd8d985ba
Eugene <samsonov_eugene@mail.ru>
2019-01-10 10:39:10 +0300

    init

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,9 +2,9 @@
 using System;
 using System.IO.Ports;
 
-namespace SerialPortExample
+namespace Program
 {
-    class SerialPortProgram
+    class Program
     { 
         private SerialPort port = new SerialPort("COM2", 9600, Parity.None, 8, StopBits.One);
         private int count = 1;
@@ -13,23 +13,52 @@ namespace SerialPortExample
 
         static void Main(string[] args)
         {
-            new SerialPortProgram();
+            new Program();
         }
 
-        private SerialPortProgram()
+        private Program()
         {
             worksheet = workbook.Worksheets.Add("Sample");
             port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
-            port.Open();
+            try
+            {
+                port.Open();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                print("Запрещен доступ к порту или выбранный вами порт уже занят другим процессом");
+            } 
+            catch (ArgumentOutOfRangeException)
+            {
+                print("Один или несколько свойств для данного экземпляра являются недопустимыми. Например " +
+                    "System.IO.Ports.SerialPort.Parity, System.IO.Ports.SerialPort.DataBits, или System.IO.Ports.SerialPort.Handshake " +
+                    "свойства не являются допустимыми; System.IO.Ports.SerialPort.BaudRate меньше " +
+                    "или равно нулю; System.IO.Ports.SerialPort.ReadTimeout или System.IO.Ports.SerialPort.WriteTimeout " +
+                    "свойство меньше нуля и не System.IO.Ports.SerialPort.InfiniteTimeout.");
+            }
+            catch(ArgumentException)
+            {
+                print("Имя порта не начинается с «COM» или тип файла порта не поддерживается.");
+            }
             Console.ReadKey();
             workbook.SaveAs("C:\\Users\\User\\Desktop\\docs\\test.xlsx");
+        }
+
+        private void print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private string bufferToString(byte[] buffer)
+        {
+            return System.Text.Encoding.Default.GetString(buffer);
         }
 
         private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             var buffer = new byte[port.ReadBufferSize];
             port.Read(buffer, 0, buffer.Length);
-            Console.WriteLine(System.Text.Encoding.Default.GetString(buffer));
+            print("new data: " + bufferToString(buffer));
             worksheet.Cell("A" + count).Value = System.Text.Encoding.Default.GetString(buffer);
             count++;
         }
